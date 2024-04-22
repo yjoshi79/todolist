5883437577d70d6edca298982c0d0de1ace1de4c
yjoshi79 <143209751+yjoshi79@users.noreply.github.com>
2024-04-22 21:49:20 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,48 @@
-const submit = document.getElementById("btnl");
-const parentlist = document.getElementById("todo");
-const clearbtn = document.getElementById("clrl");
-function fun() {
-    const input = document.getElementById("inp").value; // Use .value instead of .textContent
-    const list = document.createElement("LI");
-    const listspan = document.createElement("span");
-    listspan.textContent = input;
+const boxes = document.querySelectorAll(".box");
+const resetbtn = document.getElementById("btnl")
+const res=document.getElementById('p');
+let turn0 = true;
 
-    const clearbtn = document.createElement("button");
-    clearbtn.textContent = "Clear";
-    clearbtn.addEventListener('click', function () {
-        parentlist.removeChild(list); // Remove the list item when "Clear" button is clicked
-    });
+const winpattern = [
+    [0, 1, 2], [0, 3, 6], [0, 4, 8], [1, 4, 7], [2, 5, 8], [2, 4, 6], [3, 4, 5], [6, 7, 8]
+]
 
-    list.appendChild(listspan);
-    list.appendChild(clearbtn);
-    document.getElementById("inp").value = "";
-    parentlist.appendChild(list); // Append the list item to the parent list
-}
-function fun2() {
-    const todelete = document.getElementById('todo');
-    while (todelete.firstChild) {
-        todelete.removeChild(todelete.firstChild);
+function checkwinner() {
+    for (let pattern of winpattern) {
+        let pos1 = boxes[pattern[0]].innerText;
+        let pos2 = boxes[pattern[1]].innerText;
+        let pos3 = boxes[pattern[2]].innerText;
+        if (pos1 != "" && pos2 != "" && pos3 != "") {
+            if (pos1 === pos2 && pos2 === pos3) {
+                let msg="winner is"+ " "+pos1;
+                for(let box of boxes){
+                    box.disabled=true;
+                }
+                res.innerHTML=msg;
+            }
+        }
     }
 }
-clearbtn.addEventListener('click', fun2);
-submit.addEventListener('click', fun);
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        console.log("box was clicked");
+        if (turn0 === true) {
+            box.innerHTML = "X";
+            turn0 = false;
+        }
+        else {
+            box.innerHTML = 'O';
+            turn0 = true;
+        }
+        box.disabled = true;
+        checkwinner();
+    });
+})
+
+resetbtn.addEventListener("click", () => {
+    console.log("Reset button was clicked");
+    for (let box of boxes) {
+        box.disabled=false;
+        box.textContent = "";
+    }
+});
